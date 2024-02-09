18012e9718cd9baecf1e671caa47f7192f407a6c
Mahesh <mchouhan@ex2india.com>
2024-02-09 19:45:06 +0530

    [feat] gradient boxes created instead of images

--- src/components/AllShelves.tsx
+++ src/components/AllShelves.tsx
@@ -3,8 +3,24 @@ import { LuPlus } from "react-icons/lu"
 import AddNewShelf from "./AddNewShelf"
 import { IBookShelves } from "../types/interfaces"
 
+const backgroundColors = [
+  "linear-gradient(90deg, rgba(191,137,175,1) 0%, rgba(217,133,148,1) 100%)",
+  "linear-gradient(153deg, rgba(0,69,33,1) 0%, rgba(0,136,73,1) 100%)",
+  "linear-gradient(34deg, rgba(242,143,44,1) 0%, rgba(223,76,45,1) 100%)",
+  "background: linear-gradient(171deg, rgba(242,143,44,1) 0%, rgba(241,213,0,1) 80%)",
+  "linear-gradient(171deg, rgba(44,178,242,1) 0%, rgba(241,222,0,1) 85%)",
+  "linear-gradient(174deg, rgba(107,28,161,1) 0%, rgba(171,111,236,1) 52%, rgba(107,28,161,1) 100%)",
+  "linear-gradient(174deg, rgba(34,195,185,1) 13%, rgba(68,102,221,0.7877275910364145) 100%)",
+  "linear-gradient(174deg, rgba(221,48,83,1) 18%, rgba(205,197,27,1) 80%)"
+]
+
 const AllShelves = ({ allShelves }: { allShelves: IBookShelves[] }) => {
   const [isNewShelfClicked, setIsNewShelfClicked] = useState(false)
+  const selectBackground = () => {
+    const randomIndex =  Math.floor(Math.random() * backgroundColors.length);
+    console.log("RandomIndex : ", randomIndex);
+    return randomIndex;
+  }
   return (
     <div>
       {isNewShelfClicked ? 
@@ -13,12 +29,9 @@ const AllShelves = ({ allShelves }: { allShelves: IBookShelves[] }) => {
       <p className="text-[15px]">All Shelves</p>
         <div className="flex flex-wrap items-center">
             {allShelves.map(item => (
+      
               <div className="h-10 w-1/3 flex flex-row mb-4 gap-1" key={item.id}>
-                <img 
-                src="https://images.unsplash.com/photo-1557682260-96773eb01377?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGNvbG9yJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D"
-                alt={item.name}
-                className="w-11 h-11 rounded-sm"
-                />
+                <div className="w-11 h-11 rounded-sm" style={{background: backgroundColors[selectBackground()]}}/>
                 <div className="w-16 h-[36px] m-[1px] my-auto" style={{lineHeight: "14px"}}>
                   <p className="text-[13px] line-clamp-2">{item.name}</p>
                   <p className="text-[10px]">{`${item.articleNo} articles`}</p>
--- src/components/RecentShelves.tsx
+++ src/components/RecentShelves.tsx
@@ -1,17 +1,25 @@
 import { IBookShelves } from "../types/interfaces";
 
+const backgroundColors = [
+  "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(48,76,137,1) 71%)",
+  "linear-gradient(180deg, rgba(217,164,187,0.7120973389355743) 2%, rgba(254,174,105,1) 100%)",
+  "linear-gradient(180deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+  "linear-gradient(177deg, rgba(221,48,83,1) 35%, rgba(255,0,241,1) 80%)"
+]
+
 const RecentShelves = ({ recentShelves }: { recentShelves: IBookShelves[] }) => {
   return (
     <div className="flex flex-col gap-1 mt-1 divide-[#DBC9E5] divide-[1px]">
       <p className="text-[15px]">Recent</p>
       <div className="flex justify-between mx-3">
-        {recentShelves.map(item => (
+        {recentShelves.map((item,index) => (
           <div className="w-[66px] mr-3" key={item.id}>
-            <img
+            {/* <img
             src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvbG9yfGVufDB8fDB8fHww" 
             alt={item.name}
             className="h-14 w-14 rounded-sm"
-            />
+            /> */}
+            <div className="h-14 w-14 rounded-sm" style={{background: backgroundColors[index]}}/>
             <p className="text-[12px] w-14 my-1" style={{lineHeight: "12px"}}>
               {item.name}
             </p>
